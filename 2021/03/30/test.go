aed45e5fe7210c2525345a6f455174501e9c6d36
Johnson5218 <1392374482@qq.com>
2021-04-01 17:47:14 +0800

    [ADD] add 2021-4-1

--- 2021/03/30/test.go
+++ 2021/03/30/test.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	arr1 := []int{2,2,1}
-	ok := singleNumber(arr1)
+	arr1 := []int{1,2,2,1}
+	//arr2 := []int{2,2}
+	ok := plusOne(arr1)
 	fmt.Println(ok)
 }
 
@@ -32,4 +36,40 @@ func singleNumber(nums []int) int {
 		}
 	}
 	return 0
+}
+
+func intersect(nums1 []int, nums2 []int) []int {
+	sort.Slice(nums1, func(i, j int) bool {
+		return nums1[i] < nums1[j]
+	})
+	sort.Slice(nums2, func(i, j int) bool {
+		return nums2[i] < nums2[j]
+	})
+	res := make([]int, 0)
+	i, j := 0, 0
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] < nums2[j] {
+			i++
+		} else if nums1[i] > nums2[j] {
+			j++
+		} else {
+			res = append(res, nums1[i])
+			i++
+			j++
+		}
+	}
+	return res
+}
+
+func plusOne(digits []int) []int {
+	for i := len(digits)-1; i >= 0; i-- {
+		if digits[i] != 9 {
+			digits[i]++
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	res := append([]int{1}, digits...)
+	return res
 }
